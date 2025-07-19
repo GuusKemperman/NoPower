be619bdaa1caa28f2754db590533236b50183976
Franciszek Łuczak <223348@buas.nl>
2025-07-19 18:15:34 +0200

    Fixed character movement (for good hopefully)

--- Assets/---GAME---/Scripts/Player/PlayerAnimation.cs
+++ Assets/---GAME---/Scripts/Player/PlayerAnimation.cs
@@ -3,20 +3,23 @@ using UnityEngine;
 public class PlayerAnimation : MonoBehaviour
 {
     private PlayerMovement playerMovement = null;
+    private CharacterController controller = null;
     private float movementSpeed = 0.0f;
     private Animator animator = null;
 
     private int movementAnim = Animator.StringToHash("Movement");
+    private Rigidbody rb = null;
     
     void Start()
     {
         animator = GetComponent<Animator>();
         playerMovement = GetComponent<PlayerMovement>();
+        controller = GetComponent<CharacterController>();
         movementSpeed = playerMovement.Speed;
     }
 
     void Update()
     {
-        animator.SetFloat(movementAnim, playerMovement.velocity.magnitude / playerMovement.Speed);
+        animator.SetFloat(movementAnim, controller.velocity.magnitude / playerMovement.Speed);
     }
 }
