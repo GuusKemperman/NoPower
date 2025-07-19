6e8daca0d3550d8b26662690fb649c1d12fc422a
GuusKemperman <guus.kemperman@gmail.com>
2025-07-19 18:19:58 +0200

    Added lightning (no rendering, few bugs, no damage)

--- Assets/---GAME---/Scripts/Lighting/Lighting_ChainShot.cs
+++ Assets/---GAME---/Scripts/Lighting/Lighting_ChainShot.cs
@@ -12,18 +12,28 @@ public class Lighting_ChainShot : MonoBehaviour
     [SerializeField] GameObject LineRendererPrefab = null;
     [SerializeField] float RefreshRate = 1.0f / 60.0f;
 
-    [SerializeField] float minRayLength = 3.0f;
-    [SerializeField] float maxRayLength = 9.0f;
+    [SerializeField] float minRayLength = 1.0f;
+    [SerializeField] float maxRayLength = 3.0f;
 
     [SerializeField] float regularDeviationAngle = 15.0f;
     [SerializeField] float splittingDeviationAngle = 45.0f;
 
+    [SerializeField] float splitChancePerLength = .1f;
 
     [SerializeField] float maxTravelDist = 45.0f;
 
+    struct ChainLine
+    {
+        public ChainLine(Vector2 a_start, Vector2 a_end)
+        {
+            start = a_start;
+            end = a_end;
+        }
 
-    bool IsShooting = false;            // Holds if in the current frame the player is shooting or not.
-    bool IsLineRendererSpawned = false; // Used as flag to spawn a new line renderer on the first frame of began shooting, later frames we do not need to spawn anymore.
+        public Vector2 start;
+        public Vector2 end;
+    }
+
     List<GameObject> SpawnedLineRenderers = new List<GameObject>();
 
     void Update()
@@ -31,12 +41,7 @@ public class Lighting_ChainShot : MonoBehaviour
         InputAction AttackAction = ActionMap.FindAction("Attack");
         if (AttackAction.WasPressedThisFrame())
         {
-
-            if (PlayerEnemyDetector.GetEnemiesInRange().Count > 0)
-            {
-                if (!IsShooting) { StartShooting(); }
-            }
-            else { StopShooting(); }
+            StartShooting();
         }
 
         if (AttackAction.WasReleasedThisFrame())
@@ -45,11 +50,92 @@ public class Lighting_ChainShot : MonoBehaviour
         }
     }
 
+    UnityEngine.Vector2 RotateVec2ByAngle(UnityEngine.Vector2 vec, float angle)
+    {
+        float cosTheta = Mathf.Cos(angle * Mathf.Deg2Rad);
+        float sinTheta = Mathf.Sin(angle * Mathf.Deg2Rad);
+        return new UnityEngine.Vector2(vec.x * cosTheta - vec.y * sinTheta,
+                vec.x * sinTheta + vec.y * cosTheta);
+    }
+
     // -----------
     // Utility Functions
     // -----------
     private bool AreAllNonNull(params object[] values) => values.All(v => v != null);
     private bool AreAllTrue(params bool[] values) => values.All(v => v);
+
+    ChainLine GetNewLine(Vector2 prevStart, Vector2 prevEnd, bool isSplitter)
+    {
+        Vector2 prevDelta = prevEnd - prevStart;
+        Vector2 forward = prevDelta.normalized;
+
+        float angleDeviation = isSplitter ? Random.Range(-regularDeviationAngle, regularDeviationAngle)
+                : Random.Range(-splittingDeviationAngle, splittingDeviationAngle);
+
+        Vector2 newForward = RotateVec2ByAngle(forward, angleDeviation);
+
+        float length = Random.Range(minRayLength, maxRayLength);
+
+        return new ChainLine(prevEnd, prevEnd + newForward * length);
+    }
+
+    float Orientation(Vector2 p, Vector2 q, Vector2 r)
+    {
+        return (q.x - p.x) * (r.y - p.y) - (q.y - p.y) * (r.x - p.x);
+    }
+
+    bool AreIntersecting(ChainLine lhs, ChainLine rhs)
+    {
+        Vector2 A = lhs.start;
+        Vector2 B = lhs.end;
+        Vector2 C = rhs.start;
+        Vector2 D = rhs.end;
+
+        if (A == B || C == D)
+            return false;
+
+        float o1 = Orientation(A, B, C);
+        float o2 = Orientation(A, B, D);
+        float o3 = Orientation(C, D, A);
+        float o4 = Orientation(C, D, B);
+
+        if (o1 * o2 < 0 && o3 * o4 < 0)
+            return true;
+
+        return false;
+    }
+
+    bool IsIntersecting(List<ChainLine> list, ChainLine candidate)
+    {
+        foreach (ChainLine line in list)
+        {
+            if (AreIntersecting(line, candidate)) return true;
+        }
+        return false;
+    }
+
+    void DebugDraw(List<ChainLine> list)
+    {
+        foreach (ChainLine line in list)
+        {
+            Debug.DrawLine(new Vector3(line.start.x, .1f, line.start.y),
+                new Vector3(line.end.x, .1f, line.end.y),
+                 Color.blue, 10000.0f, false);
+        }
+    }
+
+    void SpawnLineRenderers(List<ChainLine> list)
+    {
+        foreach (ChainLine line in list)
+        {
+            GameObject NewLine = Instantiate(LineRendererPrefab);
+
+
+            Debug.DrawLine(new Vector3(line.start.x, .1f, line.start.y),
+                new Vector3(line.end.x, .1f, line.end.y),
+                 Color.blue, 10000.0f, false);
+        }
+    }
 
     // -----------
     // Shooting Functions
@@ -57,55 +143,110 @@ public class Lighting_ChainShot : MonoBehaviour
 
     private void StartShooting()
     {
-        IsShooting = true;
+        List<ChainLine> open = new List<ChainLine>();
+        List<ChainLine> closed = new List<ChainLine>();
 
-        if (AreAllNonNull(PlayerTransform, PlayerEnemyDetector, LineRendererPrefab))
+        float totalLength = minRayLength;
+        open.Add(new ChainLine(transform.position, transform.position + transform.forward * minRayLength));
+        
+        while (open.Count > 0 && totalLength < maxTravelDist)
         {
-            
-            if (!IsLineRendererSpawned) // Only doing this on the first frame of shooting
+            ChainLine curr = open[open.Count - 1];
+            open.RemoveAt(open.Count - 1);
+
+            float dist = (curr.end - curr.start).magnitude;
+            float splitChance = splitChancePerLength * dist;
+
+            for (int i = 0; i < 5; i++)
             {
-                GameObject NearestEnemy = PlayerEnemyDetector.FindClosestEnemy();
-                if (NearestEnemy != null) {
-                    IsLineRendererSpawned = true;
-                    NewLineRenderer(PlayerTransform, NearestEnemy.transform);
+                ChainLine next1 = GetNewLine(curr.start, curr.end, false);
+
+                if (IsIntersecting(open, next1) || IsIntersecting(closed, next1))
+                {
+                    continue;
                 }
-            }  
+
+                totalLength += (next1.start - next1.end).magnitude;
+                open.Add(next1);
+                break;
+            }
+
+            bool hasSplitter = UnityEngine.Random.Range(0f, 1f) < splitChance;
+
+            if (hasSplitter)
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    ChainLine next2 = GetNewLine(curr.start, curr.end, true);
+
+                    if (IsIntersecting(open, next2) || IsIntersecting(closed, next2))
+                    {
+                        continue;
+                    }
+
+                    totalLength += (next2.start - next2.end).magnitude;
+                    open.Add(next2);
+                    break;
+                }
+            }
+
+            closed.Add(curr);
         }
+
+        DebugDraw(open);
+        DebugDraw(closed);
+
+
+        //if (AreAllNonNull(PlayerTransform, PlayerEnemyDetector, LineRendererPrefab))
+        //{
+        //    LineRenderer renderer;
+
+            
+        //    if (!IsLineRendererSpawned) // Only doing this on the first frame of shooting
+        //    {
+        //        GameObject NearestEnemy = PlayerEnemyDetector.FindClosestEnemy();
+        //        if (NearestEnemy != null)
+        //        {
+        //            IsLineRendererSpawned = true;
+        //            NewLineRenderer(PlayerTransform, NearestEnemy.transform);
+        //        }
+        //    }
+        //}
     }
     private void StopShooting()
     {
-        IsShooting = false;
-        IsLineRendererSpawned = false;
+        //IsShooting = false;
+        //IsLineRendererSpawned = false;
 
-        for (int i = 0; i < SpawnedLineRenderers.Count; i++) { Destroy(SpawnedLineRenderers[i]); }
-        SpawnedLineRenderers.Clear();
+        //for (int i = 0; i < SpawnedLineRenderers.Count; i++) { Destroy(SpawnedLineRenderers[i]); }
+        //SpawnedLineRenderers.Clear();
     }
 
     // -----------
     // Line Renderer Functions
     // -----------
-    private void NewLineRenderer(Transform StartPosition, Transform EndPosition)
-    {
-        GameObject NewLine = Instantiate(LineRendererPrefab);
-        SpawnedLineRenderers.Add(NewLine);
-        StartCoroutine(UpdateLineRenderer(NewLine, StartPosition, EndPosition));
-    }
+    //private void NewLineRenderer(Transform StartPosition, Transform EndPosition)
+    //{
+    //    GameObject NewLine = Instantiate(LineRendererPrefab);
+    //    SpawnedLineRenderers.Add(NewLine);
+    //    StartCoroutine(UpdateLineRenderer(NewLine, StartPosition, EndPosition));
+    //}
 
-    IEnumerator UpdateLineRenderer (GameObject Line, Transform StartPosition, Transform EndPosition)
-    {
-        bool CheckNull = AreAllNonNull(Line, StartPosition, EndPosition);
-        bool CheckBool = AreAllTrue(IsShooting, IsLineRendererSpawned);
+    //IEnumerator UpdateLineRenderer (GameObject Line, Transform StartPosition, Transform EndPosition)
+    //{
+    //    bool CheckNull = AreAllNonNull(Line, StartPosition, EndPosition);
+    //    bool CheckBool = AreAllTrue(IsShooting, IsLineRendererSpawned);
 
-        if (CheckNull && CheckBool)
-        {
-            Line.GetComponent<Lighting_LineRendererController>().SetPosition(StartPosition, EndPosition);
-            yield return new WaitForSeconds(RefreshRate);
+    //    if (CheckNull && CheckBool)
+    //    {
+    //        Line.GetComponent<Lighting_LineRendererController>().SetPosition(StartPosition, EndPosition);
+    //        yield return new WaitForSeconds(RefreshRate);
 
-            GameObject Enemy = PlayerEnemyDetector.FindClosestEnemy();
-            if (Enemy != null) {
-                StartCoroutine(UpdateLineRenderer(Line, StartPosition, PlayerEnemyDetector.FindClosestEnemy().transform));
-            }
+    //        GameObject Enemy = PlayerEnemyDetector.FindClosestEnemy();
+    //        if (Enemy != null) {
+    //            StartCoroutine(UpdateLineRenderer(Line, StartPosition, PlayerEnemyDetector.FindClosestEnemy().transform));
+    //        }
             
-        }
-    }
+    //    }
+    //}
 }
